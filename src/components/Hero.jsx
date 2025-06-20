dc7febe3062db6f5f0a424c7fd0e5dac8e1eef41
AyushBodele <ayushbodele83@gmail.com>
2025-06-21 04:21:51 +0530

    Hero section added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { HiOutlineMapPin, HiChevronDown } from 'react-icons/hi2';
+import { FiSearch } from 'react-icons/fi';
 function Hero() {
   return (
     <>
@@ -16,6 +17,24 @@ function Hero() {
           <h1>Order food & groceries. Discover<br></br>best restaurants. Swiggy it!</h1>
         </div>
 
+        <div className="flex items-center bg-white rounded-xl px-5 py-4 shadow-md w-[300px] absolute left-90 mt-10">
+          <HiOutlineMapPin className="text-orange-600 w-5 h-5 mr-2" />
+          <input
+            type="text"
+            placeholder="Enter your delivery location"
+            className="flex-1 outline-none text-gray-500 text-base placeholder-gray-400 font-bold"
+          />
+          <HiChevronDown className="text-black w-5 h-5 ml-2" />
+        </div>
+
+        <div className="flex items-center bg-white rounded-2xl px-4 py-4 w-[500px] max-w-xl shadow-sm ml-170 mt-10">
+          <input
+            type="text"
+            placeholder="Search for restaurant, item or more"
+            className="flex-1 outline-none text-gray-500 placeholder-gray-400 bg-transparent font-bold"
+          />
+          <FiSearch className="text-gray-500 w-5 h-5" />
+      </div>
       </main>
     </>
   )
