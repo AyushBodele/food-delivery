b51a703fb0005208b774791d47b9f0ca1a6c8a53
AyushBodele <ayushbodele83@gmail.com>
2025-06-23 03:15:27 +0530

    Responsive

--- src/components/restaurant/RestaurantGrid.jsx
+++ src/components/restaurant/RestaurantGrid.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import RestaurantCard from './RestaurantCard';
 
 const RestaurantGrid = ({ restaurants }) => {
-
+{}
   return (
     <div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
